316efca394d99e552edd7efeecba34af5157cf30
serhii <ockar2018@gmail.com>
2024-12-18 00:44:04 +0200

    update project settings with necessary configurations

--- zip_airlines_core/settings.py
+++ zip_airlines_core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #third apps
+    "rest_framework",
+
+    #apps
+    "airlines_app"
 ]
 
 MIDDLEWARE = [
